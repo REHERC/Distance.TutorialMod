6e5b69b1190845b542bf89026d9a41cde62f65e0
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-07-12 00:24:43 +0200

    Add project files.

---
+++ Distance.TutorialMod/Entry.cs
@@ -0,0 +1,36 @@
+﻿using Events.MainMenu;
+using Reactor.API.Attributes;
+using Reactor.API.Interfaces.Systems;
+using Reactor.API.Logging;
+using Reactor.API.Runtime.Patching;
+using System;
+using UnityEngine;
+
+namespace Distance.TutorialMod
+{
+	[ModEntryPoint("com.github.reherc/Distance.TutorialMod")]
+	public class Mod : MonoBehaviour
+	{
+		public static Mod Instance;
+
+		public Log Logger { get; private set; }
+
+		public void Initialize(IManager manager)
+		{
+			Instance = this;
+
+			Logger = LogManager.GetForCurrentAssembly();
+
+			Logger.Info("Hello World from Tutorial Mod !");
+
+			Events.MainMenu.Initialized.Subscribe(OnMainMenuInitialized);
+
+			RuntimePatcher.AutoPatch();
+		}
+
+		private void OnMainMenuInitialized(Initialized.Data data)
+		{
+			Logger.Info("Main menu has been initialized !");
+		}
+	}
+}
---
+++ Distance.TutorialMod/Harmony/Assembly-CSharp/MainMenuLogic/OnOptionsClicked.cs
@@ -0,0 +1,22 @@
+﻿using Centrifuge.Distance.Data;
+using Centrifuge.Distance.Game;
+using HarmonyLib;
+
+namespace Distance.TutorialMod.Harmony
+{
+	[HarmonyPatch(typeof(MainMenuLogic), "OnOptionsClicked")]
+	internal static class MainMenuLogic__OnOptionsClicked
+	{
+		[HarmonyPostfix]
+		internal static void Postfix(MainMenuLogic __instance)
+		{
+			MessageBox.Create("Options was clicked", "TUTORIAL MOD")
+				.SetButtons(MessageButtons.Ok)
+				.OnConfirm(() =>
+				{
+					Mod.Instance.Logger.Info("Ok button was clicked !");
+				})
+				.Show();
+		}
+	}
+}
